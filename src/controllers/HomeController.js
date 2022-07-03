4a93a6f37d93e6c6837f60a02728ec2efcebb440
Cong Thanh <maicongthanh45261@gmail.com>
2022-07-03 14:27:58 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -125,6 +125,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === "GET_STARTED") {
+        response = { "text": "Xin chào mừng bạn ABC đến với Web đặt lịch khám bệnh của Mai Công Thành" }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
