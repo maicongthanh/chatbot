2757c9309f438a93536f181fb53b999ce6911fcc
Cong Thanh <maicongthanh45261@gmail.com>
2022-07-03 15:22:47 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -26,10 +26,30 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            response = JSON.parse(res)
+            username = `${response.first_name} ${response.last_name} `
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return username
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let response = { "text": "Xin chào mừng bạn ABC đến với Web đặt lịch khám bệnh của Mai Công Thành" }
+            let username = await getUserName(sender_psid)
+            let response = { "text": `Xin chào mừng bạn ${username} đến với Web đặt lịch khám bệnh của Mai Công Thành` }
             await callSendAPI(sender_psid, response)
             resolve('done')
         } catch (e) {
