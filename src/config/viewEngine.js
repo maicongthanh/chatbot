4d1bf9e7fa99f9202e9d217274aa36fb955d5a77
Cong Thanh <maicongthanh45261@gmail.com>
2022-07-03 12:38:17 +0700

    deloy heroku

---
+++ src/config/viewEngine.js
@@ -0,0 +1,10 @@
+import express, { application } from 'express'
+
+//config view engine for an express app
+let configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
+    app.set("view engine", "ejs")
+    app.set("views", "./src/views")
+}
+
+module.exports = configViewEngine;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import viewEngine from './config/viewEngine'
+import webRoutes from './routes/web'
+
+let app = express();
+
+//config view Engine
+viewEngine(app);
+
+//config web routes
+webRoutes(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+let port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log('App is running at the port : ' + port);
+
+})
