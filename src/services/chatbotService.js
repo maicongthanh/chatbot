68f27198133cf1bb185ebf0dc5750b3690bc36c0
Cong Thanh <maicongthanh45261@gmail.com>
2022-07-03 16:08:40 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -2,6 +2,9 @@ import request from "request"
 require('dotenv').config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
+const IMAGE_GET_START = 'bit.ly/maicongthanh-bot-1'
+
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
@@ -50,13 +53,66 @@ let handleGetStarted = (sender_psid) => {
         try {
             let username = await getUserName(sender_psid)
             let response1 = { "text": `Xin chào mừng bạn ${username} đến với Web đặt lịch khám bệnh của Mai Công Thành` }
+            let response2 = sendGetStartedTemplate();
+
+
+            //send text message
             await callSendAPI(sender_psid, response1)
+
+            //send generic template message
+
+            await callSendAPI(sender_psid, response2)
+
 
             resolve('done')
         } catch (e) {
             reject(e)
         }
     })
+}
+
+let sendGetStartedTemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Xin chào bạn đến trang web đặt lịch khám bệnh",
+                    "subtitle": "Dưới đây là các lựa chọn của chúng tôi",
+                    "image_url": IMAGE_GET_START,
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "TRANG CHỦ",
+                            "payload": "MAIN_PAGE",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "BÁC SĨ",
+                            "payload": "no",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "PHÒNG KHÁM",
+                            "payload": "no",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "ĐẶT LỊCH",
+                            "payload": "RESERVE",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "HƯỚNG DẪN SỬ DỤNG BOT",
+                            "payload": "GUIDE_TO_USE",
+                        }
+                    ],
+                }]
+            }
+        }
+    }
+    return response
 }
 
 module.exports = {
