fe4c872a289e36793b525bdfa33c20b02fde881e
Cong Thanh <maicongthanh45261@gmail.com>
2022-07-03 15:27:33 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -26,23 +26,22 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
-let getUserName = async (sender_psid) => {
-    let username = '';
-    // Send the HTTP request to the Messenger Platform
-    await request({
-        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "GET",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            response = JSON.parse(res)
-            username = `${response.first_name} ${response.last_name} `
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
-    return username
+let getUserName = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "method": "GET",
+        }, (err, res, body) => {
+            if (!err) {
+                body = JSON.parse(body)
+                username = `${body.first_name} ${body.last_name} `
+                resolve(username)
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
+    })
+
 }
 
 let handleGetStarted = (sender_psid) => {
